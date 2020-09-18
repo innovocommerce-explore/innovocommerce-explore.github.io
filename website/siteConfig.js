c6c69218540e21a5d02d84a2dc34239744f6c45b
Arturo Sevilla <arturos@innovocommerce.com>
2020-09-18 16:09:15 -0700

    Publication configuration

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'Xenon Documentation', // Title for your website.
   tagline: 'Documentation for Xenon project',
   url: 'https://innovocommerce-explore.github.io', // Your website URL
-  baseUrl: '/xe-docs/', // Base URL for your project */
+  baseUrl: '/innovocommerce-explore.github.io/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'xe-docs',
+  projectName: 'innovocommerce-explore.github.io',
   organizationName: 'innovocommerce-explore',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
